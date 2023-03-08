142969db661d5bf6586d4439775f22b8d825576c
Lars Salembier <lars.salembier@gmail.com>
2023-03-08 23:17:44 +0100

    cleanup

--- src/lib.rs
+++ src/lib.rs
@@ -43,25 +43,6 @@ impl Logger {
         Self::new(log_level::LogLevel::Info)
     }
 
-    pub fn set_min_level(&mut self, level: log_level::LogLevel) {
-        self.min_level = level;
-    }
-
-    pub fn set_filepath(&mut self, filepath: String) {
-        let log_file = log_file::LogFile::new(&filepath);
-
-        self.write_to_file = true;
-        self.log_file = Some(log_file);
-    }
-
-    pub fn set_write_to_console(&mut self, write_to_console: bool) {
-        self.write_to_console = write_to_console;
-    }
-
-    pub fn set_write_to_file(&mut self, write_to_file: bool) {
-        self.write_to_file = write_to_file;
-    }
-
     fn get_date_time() -> String {
         offset::Local::now().format("%F %T%.3f").to_string()
     }
@@ -207,68 +188,6 @@ mod tests {
             Logger::new_to_file(log_level::LogLevel::Trace, String::from("test.log"), false);
 
         assert_eq!(logger.min_level, log_level::LogLevel::Trace);
-    }
-
-    // Logger::set_level()
-
-    #[test]
-    fn set_level_should_change_min_level() {
-        let mut logger = Logger::new(log_level::LogLevel::Trace);
-
-        logger.set_min_level(log_level::LogLevel::Info);
-
-        assert_eq!(logger.min_level, log_level::LogLevel::Info);
-    }
-
-    // Logger::set_filepath()
-
-    #[test]
-    fn set_filepath_should_enable_write_to_file() {
-        let mut logger = Logger::new(log_level::LogLevel::Trace);
-
-        logger.set_filepath(String::from("test.log"));
-
-        assert_eq!(logger.write_to_file, true);
-    }
-
-    // Logger::set_write_to_console()
-
-    #[test]
-    fn set_write_to_console_should_enable_write_to_console() {
-        let mut logger = Logger::new(log_level::LogLevel::Trace);
-
-        logger.set_write_to_console(true);
-
-        assert_eq!(logger.write_to_console, true);
-    }
-
-    #[test]
-    fn set_write_to_console_should_disable_write_to_console() {
-        let mut logger = Logger::new(log_level::LogLevel::Trace);
-
-        logger.set_write_to_console(false);
-
-        assert_eq!(logger.write_to_console, false);
-    }
-
-    // Logger::set_write_to_file()
-
-    #[test]
-    fn set_write_to_file_should_enable_write_to_file() {
-        let mut logger = Logger::new(log_level::LogLevel::Trace);
-
-        logger.set_write_to_file(true);
-
-        assert_eq!(logger.write_to_file, true);
-    }
-
-    #[test]
-    fn set_write_to_file_should_disable_write_to_file() {
-        let mut logger = Logger::new(log_level::LogLevel::Trace);
-
-        logger.set_write_to_file(false);
-
-        assert_eq!(logger.write_to_file, false);
     }
 
     // Logger::get_colored_level_name()
--- src/main.rs
+++ src/main.rs
@@ -10,26 +10,4 @@ fn main() {
     logger.warning("This is a warning message");
     logger.error("This is an error message");
     logger.critical("This is a critical message");
-
-    logger.set_min_level(log_level::LogLevel::Warning);
-
-    logger.trace("This is a trace message");
-    logger.debug("This is a debug message");
-    logger.info("This is an info message");
-    logger.warning("This is a warning message");
-    logger.error("This is an error message");
-    logger.critical("This is a critical message");
-
-    logger.set_write_to_console(false);
-
-    logger.trace("This is a trace message");
-
-    logger.set_filepath("log.txt".to_string());
-
-    logger.trace("This is a trace message");
-    logger.debug("This is a debug message");
-    logger.info("This is an info message");
-    logger.warning("This is a warning message");
-    logger.error("This is an error message");
-    logger.critical("This is a critical message");
 }
